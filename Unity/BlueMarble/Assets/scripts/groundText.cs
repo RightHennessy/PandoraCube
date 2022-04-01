45b9d1219651f03b50a1c0ab30b1de6583160543
RightHennessy <myheaven_b@naver.com>
2022-04-02 02:55:21 +0900

    add : price text

---
+++ Unity/BlueMarble/Assets/scripts/groundText.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class groundText : MonoBehaviour
+{
+    public Text priceText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        priceText.text = "20,000¿ø";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
