e189bc6b755d7c08aaea1da2d99e568966157fac
RightHennessy <myheaven_b@naver.com>
2022-04-02 05:23:27 +0900

    add : dice activate

---
+++ Unity/BlueMarble/Assets/scripts/dice.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class dice : MonoBehaviour
+{
+    public GameObject diceImg;
+    Sprite[] sprites;
+    int isDice = 0;
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprites = Resources.LoadAll<Sprite>("diceImage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDice == 1)
+        {
+            StartCoroutine(Dice());
+            isDice = 0;
+        }
+    }
+
+    public void OnClickDiceButton()
+    {
+        isDice = 1;
+    }
+
+    IEnumerator Dice()
+    {
+        int diceNum = Random.Range(0,6);
+
+        for(double f = 3f; f>=0; f -= 0.1)
+        {
+            index = Random.Range(0,6);
+            diceImg.GetComponent<Image>().sprite = sprites[index];
+
+            yield return new WaitForSeconds(.1f);
+        }
+
+        diceImg.GetComponent<Image>().sprite = sprites[diceNum];
+
+    }
+    
+}
